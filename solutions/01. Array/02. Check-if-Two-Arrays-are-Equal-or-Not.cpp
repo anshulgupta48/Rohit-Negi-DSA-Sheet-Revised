067e9346e7879d6c6395dc54717800054f998b27
anshulgupta48 <anshulgupta4905@gmail.com>
2025-06-15 18:31:45 +0530

    Day-1

---
+++ solutions/01. Array/02. Check-if-Two-Arrays-are-Equal-or-Not.cpp
@@ -0,0 +1,31 @@
+// ******** Given two arrays a[] and b[] of equal size, the task is to find whether the elements in the arrays are equal. Two arrays are said to be equal if both contain the same set of elements, arrangements (or permutations) of elements may be different though. ********
+// Note --> If there are repetitions, then counts of repeated elements must also be the same for two arrays to be equal.
+
+// <======== Example ========>
+// Input: a[] = [1, 2, 5, 4, 0], b[] = [2, 4, 5, 0, 1]
+// Output: true
+
+// Input: a[] = [1, 2, 5], b[] = [2, 4, 15]
+// Output: false
+
+
+
+
+class Solution{
+    public:
+    bool checkEqual(vector<int>& a, vector<int>& b) {
+        int n = a.size();
+        int m = b.size();
+        map<int, int> freqA;
+        map<int, int> freqB;
+        
+        for(int i = 0; i < n; i++) {
+            freqA[a[i]]++;
+        }
+        for(int i = 0; i < m; i++) {
+            freqB[b[i]]++;
+        }
+        
+        return (freqA == freqB);
+    }
+};
