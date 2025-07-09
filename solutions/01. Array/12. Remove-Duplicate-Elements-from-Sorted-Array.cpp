7c8f1163bd9fc317a966840ba6f05d1cfb232b57
anshulgupta48 <anshulgupta4905@gmail.com>
2025-07-09 09:57:00 +0530

    Day-3

---
+++ solutions/01. Array/12. Remove-Duplicate-Elements-from-Sorted-Array.cpp
@@ -0,0 +1,29 @@
+// ******** Given a sorted array arr. Return the size of the modified array which contains only distinct elements. ********
+// Note --> Don't use set or HashMap to solve the problem. You must return the modified array size only where distinct elements are present and modify the original array such that all the distinct elements come at the beginning of the original array.
+
+// <======== Example ========>
+// Input: arr = [2, 2, 2, 2, 2]
+// Output: [2]
+
+// Input: arr = [1, 2, 4]
+// Output: [1, 2, 4]
+
+
+
+
+class Solution{
+    public:
+    int removeDuplicates(vector<int> &arr) {
+        int n = arr.size();
+        int i = 0;
+        
+        for(int j = 0; j < n-1; j++) {
+            if(arr[j] != arr[j+1]) {
+                arr[i++] = arr[j];
+            }
+        }
+        
+        arr[i++] = arr[n-1];
+        return i;
+    }
+};
