3b2c6795d5b7015a66ddbb92784c4d292908aae2
anshulgupta48 <anshulgupta4905@gmail.com>
2025-08-31 14:25:25 +0530

    Day-10

---
+++ solutions/01. Array/35. Count-Primes.cpp
@@ -0,0 +1,37 @@
+// ******** Given an integer n, return the number of prime numbers that are strictly less than n. ********
+
+// <======== Example ========>
+// Input: n = 10
+// Output: 4
+
+// Input: n = 0
+// Output: 0
+
+// Input: n = 1
+// Output: 0
+
+
+// Expected Time Complexity ==> O(N2)
+// Expected Auxiliary Space ==> O(1)
+
+
+
+
+class Solution{
+    public:
+    int countPrimes(int n) {
+        int count = 0;
+        vector<bool> isPrime(n, true);
+
+        for(int i = 2; i < n; i++) {
+            if(isPrime[i] == true) {
+                count++;
+                for(int j = 2*i; j < n; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+
+        return count;
+    }
+};
