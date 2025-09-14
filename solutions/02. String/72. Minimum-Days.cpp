8946204803a2f60576b27b4055bd7b9ec8a49f5b
anshulgupta48 <anshulgupta4905@gmail.com>
2025-09-14 12:27:38 +0530

    Day-23

---
+++ solutions/02. String/72. Minimum-Days.cpp
@@ -0,0 +1,48 @@
+// ******** Given a string S of length N containing only lowercase alphabets. You are also given a permutation P of length N containing integers from 0 to N-1. In (i+1)'th day you can take the P[i] value of the permutation array and replace S[P[i]] with a '?'. You have to tell the minimum number of days required, such that after it for all index i (0<=i<N-1), if S[i]!='?', then S[i]!=S[i+1]. ********
+
+// <======== Example ========>
+// Input: N = 4, S = "aabb", P[] = {2, 1, 3, 0}
+// Output: 2
+
+// Input: N = 4, S = "abca", P[] = {3, 0, 2, 1}
+// Output: 0
+
+
+// Expected Time Complexity ==> O(N)
+// Expected Auxiliary Space ==> O(1)
+
+
+
+
+class Solution{
+    public:
+    int getMinimumDays(int N, string S, vector<int> &P) {
+        int count = 0;
+        
+        for(int i = 0; i < N-1; i++) {
+            if(S[i] == S[i+1]) {
+                count++;
+            }
+        }
+        
+        if(count == 0) {
+            return 0;
+        }
+        
+        for(int i = 0; i < N; i++) {
+            if(P[i]-1 >= 0 && S[P[i]-1] == S[P[i]]) {
+                count--;
+            }
+            if(P[i]+1 >= 0 && S[P[i]+1] == S[P[i]]) {
+                count--;
+            }
+            
+            S[P[i]] = '?';
+            if(count == 0) {
+                return i+1;
+            }
+        }
+        
+        return N;
+    }
+};
