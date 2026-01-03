4d0d89a73d71186430b84be946e12cf3e47b043b
anshulgupta48 <anshulgupta4905@gmail.com>
2026-01-03 18:25:34 +0530

    Day-10

---
+++ solutions/01. Array/35. Count-Primes.cpp
@@ -0,0 +1,38 @@
+// ******** Given an integer n, return the number of prime numbers that are strictly less than n. ********
+
+// <======== Example ========>
+// Input: n = 10
+// Output: 4
+
+// Input: n = 0
+// Output: 0
+
+// Input: n = 1
+// Output: 0
+
+
+// Expected Time Complexity ==> O(n*logn)
+// Expected Auxiliary Space ==> O(n)
+
+
+
+
+class Solution{
+    public:
+    int countPrimes(int n) {
+        int count = 0;
+        vector<bool> isPrime(n, true);
+
+        for(int i = 2; i < n; i++) {
+            if(isPrime[i] == true) {
+                count++;
+
+                for(int j = 2*i; j < n; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+
+        return count;
+    }
+};
---
+++ solutions/01. Array/37. Arithmetic-Subarrays.cpp
@@ -0,0 +1,44 @@
+// ******** A sequence of numbers is called arithmetic if it consists of at least two elements, and the difference between every two consecutive elements is the same. More formally, a sequence s is arithmetic if and only if s[i+1] - s[i] == s[1] - s[0] for all valid i. You are given an array of n integers, nums, and two arrays of m integers each, l and r, representing the m range queries, where the ith query is the range [l[i], r[i]]. All the arrays are 0-indexed. Return a list of boolean elements answer, where answer[i] is true if the subarray nums[l[i]], nums[l[i]+1], ... , nums[r[i]] can be rearranged to form an arithmetic sequence, and false otherwise. ********
+
+// <======== Example ========>
+// Input: nums = [4,6,5,9,3,7], l = [0,0,2], r = [2,3,5]
+// Output: [true,false,true]
+
+// Input: nums = [-12,-9,-3,-12,-6,15,20,-25,-20,-15,-10], l = [0,1,6,4,8,7], r = [4,4,9,7,9,10]
+// Output: [false,true,false,false,true,true]
+
+
+// Expected Time Complexity ==> O(n)
+// Expected Auxiliary Space ==> O(n)
+
+
+
+
+class Solution{
+    public:
+    bool checkSubarrays(vector<int>& arr) {
+        int n = arr.size();
+        sort(arr.begin(), arr.end());
+        int diff = arr[1] - arr[0];
+
+        for(int i = 1; i < n; i++) {
+            if(arr[i]-arr[i-1] != diff) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    vector<bool> checkArithmeticSubarrays(vector<int>& nums, vector<int>& l, vector<int>& r) {
+        int n = l.size();
+        vector<bool> ans;
+
+        for(int i = 0; i < n; i++) {
+            vector<int> temp(nums.begin()+l[i], nums.begin()+r[i]+1);
+            ans.push_back(checkSubarrays(temp));
+        }
+
+        return ans;
+    }
+};
