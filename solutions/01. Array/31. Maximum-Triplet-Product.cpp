16536201156164cdbb3fd999a9abdda9917b9061
anshulgupta48 <anshulgupta4905@gmail.com>
2026-01-03 15:42:14 +0530

    Day-8

---
+++ solutions/01. Array/31. Maximum-Triplet-Product.cpp
@@ -0,0 +1,50 @@
+// ******** Given an array arr of size n, the task is to find the maximum triplet product in the array. ********
+
+// <======== Example ========>
+// Input: n = 4, arr[] = {1, 2, 3, 5}
+// Output: 30
+
+// Input: n = 7, arr[] = {-3, -5, 1, 0, 8, 3, -2}
+// Output: 120
+
+
+// Expected Time Complexity ==> O(n)
+// Expected Auxiliary Space ==> O(1)
+
+
+
+
+class Solution{
+    public:
+    long long maxTripletProduct(long long arr[], int n) {
+        long long firstMin = INT_MAX, secondMin = INT_MAX;
+        long long firstMax = INT_MIN, secondMax = INT_MIN, thirdMax = INT_MIN;
+        
+        for(int i = 0; i < n; i++) {
+            if(arr[i] < firstMin) {
+                secondMin = firstMin;
+                firstMin = arr[i];
+            }
+            else if(arr[i] < secondMin) {
+                secondMin = arr[i];
+            }
+            
+            if(arr[i] > firstMax) {
+                thirdMax = secondMax;
+                secondMax = firstMax;
+                firstMax = arr[i];
+            }
+            else if(arr[i] > secondMax) {
+                thirdMax = secondMax;
+                secondMax = arr[i];
+            }
+            else if(arr[i] > thirdMax) {
+                thirdMax = arr[i];
+            }
+        }
+        
+        long long triplet1 = firstMin * secondMin * firstMax;
+        long long triplet2 = firstMax * secondMax * thirdMax;
+        return max(triplet1, triplet2);
+    }
+};
---
+++ solutions/01. Array/32. Coins-of-Geekland.cpp
@@ -0,0 +1,37 @@
+// ******** In Geekland there is a grid of coins of size N x N. You have to find the maximum sum of coins in any sub-grid of size K x K. ********
+// Note --> Coins of the negative denomination are also possible at Geekland.
+
+// <======== Example ========>
+// Input: N = 5, K = 3, mat[[]] = {{1, 1, 1, 1, 1}, {2, 2, 2, 2, 2}, {3, 8, 6, 7, 3}, {4, 4, 4, 4, 4}, {5, 5, 5, 5, 5}}
+// Output: 48
+
+// Input: N = 1, K = 1, mat[[]] = {{4}} 
+// Output: 4
+
+
+// Expected Time Complexity ==> O(N2)
+// Expected Auxiliary Space ==> O(N2)
+
+
+
+
+class Solution{
+    public:
+    int Maximum_Sum(vector<vector<int>> &mat, int N, int K) {
+        int maxSum = INT_MIN;
+        int temp[N+1][N+1] = {0};
+        
+        for(int i = 1; i <= N; i++) {
+            for(int j = 1; j <= N; j++) {
+                temp[i][j] = mat[i-1][j-1] + temp[i-1][j] + temp[i][j-1] -temp[i-1][j-1];
+                
+                if(i >= K && j >= K) {
+                    int sum = temp[i][j] + temp[i-K][j-K] - temp[i-K][j] - temp[i][j-K];
+                    maxSum = max(maxSum, sum);
+                }
+            }
+        }
+        
+        return maxSum;
+    }
+};
