9794006a12888bef78a35f7a817c59ecc9c4b646
anshulgupta48 <anshulgupta4905@gmail.com>
2026-07-24 10:02:35 +0530

    Day-17

---
+++ solutions/02. String/57. Longest-Substring-with-Distinct-Characters.cpp
@@ -0,0 +1,46 @@
+// ******** Given a string s, find the length of the longest substring with all distinct characters. ********
+
+// <======== Example ========>
+// Input: s = "geeksforgeeks"
+// Output: 7
+
+// Input: s = "aaa"
+// Output: 1
+
+// Input: s = "abcdefabcbb"
+// Output: 6
+
+
+// Expected Time Complexity ==> O(n)
+// Expected Auxiliary Space ==> O(n)
+
+
+
+
+class Solution{
+    public:
+    int longestUniqueSubstr(string &s) {
+        int n = s.size();
+        int i = 0;
+        int ans = 0;
+        map<char, int> mp;
+        
+        while(i < n) {
+            if(mp.count(s[i]) == 0) {
+                mp.insert({s[i], i});
+                i++;
+            } else {
+                int temp = mp.size();
+                ans = max(ans, temp);
+                i = mp[s[i]] + 1;
+                mp.clear();
+            }
+        }
+        
+        if(ans < mp.size()) {
+            ans = mp.size();
+        }
+        
+        return ans;
+    }
+};
