5b2b12b92e0be3557dfab8f7225cf7749da626b1
anshulgupta48 <anshulgupta4905@gmail.com>
2026-08-14 21:53:28 +0530

    Day-36

---
+++ solutions/03. Searching-and-Sorting/114. Square-Root.cpp
@@ -0,0 +1,45 @@
+// ******** Given a positive integer n, find the square root of n. If n is not a perfect square, then return the floor value. Floor value of any number is the greatest Integer which is less than or equal to that number. ********
+
+// <======== Example ========>
+// Input: n = 4
+// Output: 2
+
+// Input: n = 11
+// Output: 3
+
+// Input: n = 1
+// Output: 1
+
+
+// Expected Time Complexity ==> O(logn)
+// Expected Auxiliary Space ==> O(1)
+
+
+
+
+class Solution{
+    public:
+    int floorSqrt(int n) {
+        int start = 0;
+        int end = n-1;
+        int ans = -1;
+        
+        if(n == 0 || n == 1) {
+            return n;
+        }
+        
+        while(start <= end) {
+            int mid = start + (end - start) / 2;
+            int square = (mid * mid);
+            
+            if(square <= n) {
+                ans = mid;
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        
+        return ans;
+    }
+};
